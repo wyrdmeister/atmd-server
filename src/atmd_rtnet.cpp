c30b94d6f734316c9a44503bb817eac83853f782
Michele Devetta <michele.devetta@mail.polimi.it>
2012-01-25 09:20:54 +0100

    Added atmd_rtnet.cpp/h. Added debug to RTnet class.

---
+++ src/atmd_rtnet.cpp
@@ -0,0 +1,204 @@
+/*
+ * ATMD Server version 3.0
+ *
+ * ATMD Server - Virtual board
+ *
+ * Copyright (C) Michele Devetta 2012 <michele.devetta@unimi.it>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Global termination interrupt
+extern bool terminate_interrupt;
+
+// Global debug flag
+#ifdef DEBUG
+extern bool enable_debug;
+#endif
+
+#include "atmd_rtnet.h"
+
+
+/* @fn int RTnet::init(bool en_bind = false)
+ *
+ */
+int RTnet::init(bool en_bind) {
+
+  int retval = 0;
+
+  // Check that the interface name is set
+  if(en_bind && strlen(_ifname) == 0) {
+    rt_syslog(ATMD_ERR, "RTnet [init]: cannot bind without specifing the interface name.");
+    return -1;
+  }
+
+  // Check that the protocol type is specified
+  if(_protocol == ATMD_PROTO_NONE) {
+    rt_syslog(ATMD_ERR, "RTnet [init]: cannot proceed without specifing the protocol ID.");
+    return -1;
+  }
+
+  // Create real-time network control socket
+  _sock = rt_dev_socket(AF_PACKET, SOCK_DGRAM, htons(_protocol));
+  if (_sock < 0) {
+    rt_syslog(ATMD_ERR, "RTnet [init]: cannot create RT socket. Error: '%s'.\n", strerror(-_sock));
+    return -1;
+  }
+
+#ifdef DEBUG
+  if(enable_debug)
+    rt_syslog(ATMD_DEBUG, "RTnet [init]: successfully created RT socket with descriptor '%d'.\n", _sock);
+#endif
+
+  // Extend the socket pool
+  retval = rt_dev_ioctl(_sock, RTNET_RTIOC_EXTPOOL, _rtskbs);
+  if (retval != (int)_rtskbs) {
+    rt_syslog(ATMD_ERR, "RTnet [init]: rt_dev_ioctl(RT_IOC_SO_EXTPOOL) failed. Error: '%s'\n", strerror(-retval));
+    rt_dev_close(_sock);
+    _sock = -1;
+    return -1;
+  }
+
+#ifdef DEBUG
+  if(enable_debug)
+    rt_syslog(ATMD_DEBUG, "RTnet [init]: successfully extended RT socket pool to %d SKBS.\n", _rtskbs);
+#endif
+
+  // If bind is enabled...
+  if(en_bind) {
+
+    // Find interface ID
+    struct ifreq ifr;
+    strncpy(ifr.ifr_name, _ifname, IFNAMSIZ);
+    retval = rt_dev_ioctl(_sock, SIOCGIFINDEX, &ifr);
+    if(retval < 0) {
+      rt_syslog(ATMD_ERR, "RTnet [init]: cannot identify interface '%s'. Error: '%s'.", _ifname, strerror(-retval));
+      rt_dev_close(_sock);
+      _sock = -1;
+      return -1;
+    }
+
+#ifdef DEBUG
+  if(enable_debug)
+    rt_syslog(ATMD_DEBUG, "RTnet [init]: interface '%s' was found to have ID %d.\n", _ifname, ifr.ifr_ifindex);
+#endif
+
+    // Local address
+    struct sockaddr_ll local_addr;
+    memset(&local_addr, 0, sizeof(struct sockaddr_ll));
+    local_addr.sll_family = AF_PACKET;
+    local_addr.sll_protocol = htons(_protocol);
+    local_addr.sll_ifindex = ifr.ifr_ifindex;
+
+    // Bind socket
+    retval = rt_dev_bind(_sock, (struct sockaddr *)&local_addr, sizeof(struct sockaddr_ll));
+    if(retval < 0) {
+      rt_syslog(ATMD_ERR, "RTnet [init]: cannot bind socket to local interface '%s'. Error: '%s'.", _ifname, strerror(-retval));
+      rt_dev_close(_sock);
+      _sock = -1;
+      return -1;
+    }
+
+#ifdef DEBUG
+    if(enable_debug)
+      rt_syslog(ATMD_DEBUG, "RTnet [init]: successfully binded RT socket '%d' to interface '%s' with protocol '0x%04X'.\n", _sock, _ifname, _protocol);
+#endif
+  }
+
+  return 0;
+}
+
+
+/* @fn int RTnet::send(const GenMsg& packet, struct ether_addr* address)
+ *
+ */
+void RTnet::send(const GenMsg& packet, const struct ether_addr* addr)const throw(int) {
+  // Compose address
+  struct sockaddr_ll remote_addr;
+  memset(&remote_addr, 0, sizeof(struct sockaddr_ll));
+  remote_addr.sll_family = AF_PACKET;
+  remote_addr.sll_protocol = _protocol;
+  memcpy(&remote_addr.sll_addr, addr, sizeof(struct ether_addr));
+
+  // Send message
+  int retval = rt_dev_sendto(_sock, packet.get_buffer(), packet.size(), 0, (struct sockaddr*)&remote_addr, sizeof(struct sockaddr_ll));
+  if(retval < 0) {
+    rt_syslog(ATMD_ERR, "RTnet [send]: failed to send packet. Error: '%s'.", strerror(-retval));
+    throw(retval);
+  }
+}
+
+
+/* @fn int RTnet::recv(GenMsg& packet, struct ether_addr* remote_addr = NULL)
+ *
+ */
+void RTnet::recv(GenMsg& packet, struct ether_addr* addr)const throw(int) {
+  // Clear packet
+  packet.clear();
+
+  // Init remote addr structure
+  struct sockaddr_ll remote_addr;
+  socklen_t remote_len = sizeof(struct sockaddr_ll);
+  memset(&remote_addr, 0, sizeof(struct sockaddr_ll));
+
+  // Receive packet
+  int retval = rt_dev_recvfrom(_sock, packet.get_buffer(), packet.maxsize(), 0, (struct sockaddr*)&remote_addr, &remote_len);
+  if(retval < 0) {
+    rt_syslog(ATMD_ERR, "RTnet [recv]: failed to receive packet. Error: '%s'.", strerror(-retval));
+    throw(retval);
+  }
+
+  // Copy remote addr
+  if(addr) {
+    memcpy(&addr, &remote_addr.sll_addr, sizeof(struct ether_addr));
+  }
+}
+
+
+/* @fn unsigned long RTnet::wait_tdma(unsigned long cycle)
+ * Wait for a specific TDMA cycle
+ */
+unsigned long RTnet::wait_tdma(unsigned long cycle) {
+  unsigned long curr = 0;
+  while(true) {
+    curr = wait_tdma();
+    if(curr < cycle)
+      continue;
+    else if(curr == cycle)
+      return curr;
+    else {
+      rt_syslog(ATMD_ERR, "RTnet [recv]: tried to sync on a TDMA cycle in the past.");
+      return curr;
+    }
+  }
+}
+
+
+/* @fn unsigned long RTnet::wait_tdma()
+ * Wait for next TDMA cycle
+ */
+unsigned long RTnet::wait_tdma() {
+  struct rtmac_waitinfo waitinfo;
+  waitinfo.type = TDMA_WAIT_ON_SYNC;
+  waitinfo.size = sizeof(waitinfo);
+
+  int retval = rt_dev_ioctl(_sock, RTMAC_RTIOC_WAITONCYCLE_EX, &waitinfo);
+  if(retval) {
+    rt_syslog(ATMD_ERR, "RTnet [wait_tdma]: error in IOCTL waiting for TMDA sync. Error: %s.", strerror(-retval));
+    return 0;
+
+  } else {
+    return waitinfo.cycle_no;
+  }
+}
---
+++ src/atmd_rtnet.h
@@ -0,0 +1,104 @@
+/*
+ * ATMD Server version 3.0
+ *
+ * ATMD Server - RTnet class header
+ *
+ * Copyright (C) Michele Devetta 2012 <michele.devetta@unimi.it>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ATMD_RTNET_H
+#define ATMD_RTNET_H
+
+// Default number of RTSKBS
+#define ATMD_DEF_RTSKBS     250
+
+// Protocols
+#define ATMD_PROTO_NONE     0x0000
+#define ATMD_PROTO_CTRL     0x5555
+#define ATMD_PROTO_DATA     0x5115
+
+// Global
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <netinet/ether.h>
+#include <netpacket/packet.h>
+
+// Xenomai
+#include <rtdk.h>
+#include <rtnet.h>
+#include <rtmac.h>
+
+// Local
+#include "common.h"
+#include "atmd_netagent.h"
+
+
+/* @class RTnet
+ * Define a wrapper around an RTnet socket
+ */
+class RTnet {
+public:
+  RTnet() : _sock(-1), _rtskbs(ATMD_DEF_RTSKBS), _protocol(ATMD_PROTO_NONE) { memset(_ifname, 0, IFNAMSIZ); };
+  ~RTnet() { if(_sock) rt_dev_close(_sock); };
+
+  // Configure RTSKBS
+  void rtskbs(unsigned int num) { _rtskbs = num; };
+  unsigned int rtskbs()const { return _rtskbs; };
+
+  // Configure interface
+  void interface(const char* name) { strncpy(_ifname, name, IFNAMSIZ); _ifname[IFNAMSIZ-1] = '\0'; };
+  const char* interface()const { return _ifname; };
+
+  // Configure protocol
+  void protocol(uint16_t proto) { _protocol = proto; };
+  uint16_t protocol()const { return _protocol; };
+
+  // Init socket
+  int init(bool en_bind = false);
+
+  // Send packet
+  void send(const GenMsg& packet, const struct ether_addr* addr)const throw(int);
+
+  // Receive packet
+  void recv(GenMsg& packet, struct ether_addr* addr = NULL)const throw(int);
+
+  // Close socket
+  void close() { rt_dev_close(_sock); };
+
+  // Wait for a TDMA cycle
+  unsigned long wait_tdma(unsigned long cycle);
+
+  // Wait on TDMA sync
+  unsigned long wait_tdma();
+
+private:
+  // Socket
+  int _sock;
+
+  // Number of SKBS
+  unsigned int _rtskbs;
+
+  // Interface name for bind
+  char _ifname[IFNAMSIZ];
+
+  // Protocol for bind
+  uint16_t _protocol;
+};
+
+#endif
